4550c321b7a7eb91d00b8565d2e8adc465206da3
junyong <rlawnsdyd530@gmail.com>
2025-07-06 02:39:16 +0900

    feat : 공고 리스트 swagger 제목 작성

--- src/main/java/emergency/server/controller/AnnouncementController.java
+++ src/main/java/emergency/server/controller/AnnouncementController.java
@@ -5,6 +5,7 @@ import emergency.server.domain.enums.Target;
 import emergency.server.dto.AnnouncementResponseDTO;
 import emergency.server.global.common.apiPayload.ApiResponse;
 import emergency.server.service.AnnouncementService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,10 +19,11 @@ public class AnnouncementController {
     private final AnnouncementService announcementService;
 
     @GetMapping ("/")// 모집공고 리스트 조회
+    @Operation(summary = "홈화면 공고 리스트 조회 API", description = "type은 필수, 그 외의 사항들은 필수값이 아닙니다")
     public ApiResponse<List<AnnouncementResponseDTO.AnnouncementListDto>> announcementList(
             @RequestParam AnnouncementType type,
-            @RequestParam Boolean isRecruiting,
-            @RequestParam Long regionId,
+            @RequestParam(required = false) Boolean isRecruiting,
+            @RequestParam(required = false) Long regionId,
             @RequestParam(required = false) Target target) {
         List<AnnouncementResponseDTO.AnnouncementListDto> announcementList
                 = announcementService.getAnnouncementList(type, isRecruiting, regionId, target);
