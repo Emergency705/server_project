fa4c6cb20cbdc5e8c90bd42f83f1185fc9a0f444
woojoo48 <wjddnwn0408@gmail.com>
2025-07-06 02:38:23 +0900

    Feat : 로그인, 정보 조회 DTO 추가

--- src/main/java/emergency/server/dtos/UserRequestDto.java
+++ src/main/java/emergency/server/dtos/UserRequestDto.java
@@ -3,6 +3,7 @@ package emergency.server.dtos;
 import emergency.server.domain.Region;
 import emergency.server.domain.enums.DisableType;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,5 +20,14 @@ public class UserRequestDto {
         LocalDate birth;
         DisableType disableType;
         Long regionId;
+    }
+
+    @Getter
+    @Setter
+    public static class LoginRequestDTO{
+        @NotBlank(message = "이메일은 필수입니다.")
+        private String loginId;
+        @NotBlank(message = "패스워드는 필수입니다.")
+        private String password;
     }
 }
--- src/main/java/emergency/server/dtos/UserResponseDto.java
+++ src/main/java/emergency/server/dtos/UserResponseDto.java
@@ -1,10 +1,11 @@
 package emergency.server.dtos;
 
+import emergency.server.domain.enums.DisableType;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class UserResponseDto {
@@ -16,5 +17,27 @@ public class UserResponseDto {
     public static class JoinResultDto{
         Long userId;
         LocalDateTime createdAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class LoginResultDTO {
+        Long memberId;
+        String accessToken;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UserInfoDto {
+        Long userId;
+        String name;
+        String loginId;
+        LocalDate birth;
+        DisableType disableType;
+        String regionName;
     }
 }
